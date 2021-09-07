98393b9acca03b53ef6a8c371cc47b0ae8d9a7fa
Karthi <keyan2663@gmail.com>
2021-09-07 11:06:25 +0530

    First commit

--- hello.java
+++ hello.java
@@ -2,8 +2,6 @@ import java.util.*;
 class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello");
-		System.out.println("Hello 2");
-		System.out.println("commit after updated file");
 	}
 }	
 
